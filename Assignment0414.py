19fd5f9e81537cea8f89f8a5283257bd42d8374a
HuaIvy <103197696+HuaIvy@users.noreply.github.com>
2022-04-18 11:27:41 +0800

    Update Assignment0414.py

--- Assignment0414.py
+++ Assignment0414.py
@@ -26,26 +26,7 @@ print("程式執行完畢")
 
 # print('******作業2 因數分解******')
 
-# number = int(input("請輸入數字"))
 
-# def reduceNum(n):
-#     print ('{} = '.format(n), end=" ")
-#     if not isinstance(n, int) or n <= 0 :
-#         print ('請輸入一個正確的數字 !')
-#         exit(0)
-#     elif n in [1] :
-#         print ('{}'.format(n))
-#     while n not in [1] : # 迴圈保證遞迴
-#         for index in range(2, n + 1) :
-#             if n % index == 0:
-#                 n //= index # n 等於 n//index
-#                 if n == 1: 
-#                     print (index )
-#                 else : # index 一定是素數
-#                     print ('{} *'.format(index), end=" ")
-#                 break
-
-# reduceNum(number)
 
 print('*****作業3 有四個數字，1,2,3,4 能組成多少個互不相同且無重覆數字的三位數，各是多少**************')
 import itertools
